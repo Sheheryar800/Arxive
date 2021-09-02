0ad243218ebab975f8ecd955a4033041fe96c335
HM TAhir <tahir4work@gmail.com>
2021-09-02 18:01:20 +0500

    random articals functionality added

---
+++ build/precache-manifest.25a1f2a4e2f3b15dd95f75cf4ac95865.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a545a2f2536f20969fc0cf17aa356b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15c88be699c08ba9afbb",
+    "url": "/static/css/2.5b039a8e.chunk.css"
+  },
+  {
+    "revision": "feeb25bace51a421cec5",
+    "url": "/static/css/main.a6e822bc.chunk.css"
+  },
+  {
+    "revision": "15c88be699c08ba9afbb",
+    "url": "/static/js/2.769365c1.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/static/js/2.769365c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feeb25bace51a421cec5",
+    "url": "/static/js/main.a1cdc062.chunk.js"
+  },
+  {
+    "revision": "d96159fb79fad195664d",
+    "url": "/static/js/runtime-main.2dc790a7.js"
+  },
+  {
+    "revision": "294406386688a4c3ee996bf4bd7d27bf",
+    "url": "/static/media/BSA.29440638.gif"
+  },
+  {
+    "revision": "6bdd6adba12ea1fbe6404ece8215ad08",
+    "url": "/static/media/BSA_Logo.6bdd6adb.png"
+  },
+  {
+    "revision": "4d978fa28a0db8bbc74b6e29ed2bf883",
+    "url": "/static/media/duffy.4d978fa2.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "f28ac2997fe2fb1d682dff077604183c",
+    "url": "/static/media/neeraj.f28ac299.JPG"
+  }
+]);
